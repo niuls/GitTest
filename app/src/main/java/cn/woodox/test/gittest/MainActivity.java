44bf311e2bbc011652db0ab70bbcdf9ba201152e
niuls <niuls@vip.qq.com>
2015-07-17 18:03:01 +0800

    modified at work.

--- app/src/main/java/cn/woodox/test/gittest/MainActivity.java
+++ app/src/main/java/cn/woodox/test/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		System.out.printf("modified at work. 7.17");
 	}
 }
